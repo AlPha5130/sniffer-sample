2e7b9bef9eb67e43860e9c81e6fe9d1bb22107bf
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2023-03-19 20:24:43 +0800

    UI, basic monitoring

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SharpPcap;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,27 +22,113 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly CaptureDeviceList devices;
+        private ILiveDevice device;
+        public ObservableCollection<PacketRecord> Records { get; private set; }
+        private bool captureRunning = false;
+
+        private bool backgroundThreadStop = true;
+        private readonly object queueLock = new();
+        private Thread backgroundThread;
+
+        private List<RawCapture> captureList = new();
+        private Queue<PacketRecord> packetQueue = new();
+        private int packetCount = 0;
+        private DateTime lastStatisticsOutput = DateTime.MinValue;
+        private readonly TimeSpan updateStatisticsInterval = new(0, 0, 1);
+        private bool statisticsNeedsUpdate = false;
+        private ICaptureStatistics captureStatistics;
+
         public MainWindow()
         {
             InitializeComponent();
+            Records = new();
+            PacketList.ItemsSource = Records;
+            devices = CaptureDeviceList.Instance;
+            if (devices.Count < 1)
+            {
+                MessageBox.Show("No devices found.");
+                Application.Current.Shutdown();
+                return;
+            }
+            foreach (var device in devices)
+            {
+                InterfaceComboBox.Items.Add(device.Description);
+                InterfaceComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void AddPacketRecord(object s, PacketCapture e)
+        {
+            var now = DateTime.Now;
+            var interval = now - lastStatisticsOutput;
+            if (interval > updateStatisticsInterval)
+            {
+                statisticsNeedsUpdate = true;
+                captureStatistics = e.Device.Statistics;
+                lastStatisticsOutput = now;
+            }
+            lock (queueLock)
+            {
+                captureList.Add(e.GetPacket());
+            }
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
+            packetCount = 0;
+            packetQueue = new();
+            Records.Clear();
+            lastStatisticsOutput = DateTime.Now;
 
+            StartCapture();
+
+            StartButton.IsEnabled = false;
+            StopButton.IsEnabled = true;
+            ClearButton.IsEnabled = false;
+        }
+
+        private void StartCapture()
+        {
+            backgroundThreadStop = false;
+            backgroundThread = new Thread(BackgroundThread);
+            backgroundThread.Start();
+
+            device.OnPacketArrival += AddPacketRecord;
+            device.Open();
+
+            captureStatistics = device.Statistics;
+            UpdateStatistics();
+
+            device.StartCapture();
+            captureRunning = true;
+        }
+
+        private void StopCapture()
+        {
+            if (captureRunning)
+            {
+                device.StopCapture();
+                device.Close();
+                device.OnPacketArrival -= AddPacketRecord;
+
+                backgroundThreadStop = true;
+                backgroundThread.Join();
+
+                captureRunning = false;
+            }
         }
 
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
+            StopCapture();
 
+            StartButton.IsEnabled = true;
+            StopButton.IsEnabled = false;
+            ClearButton.IsEnabled = true;
         }
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ProtocolComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
@@ -57,7 +145,72 @@ namespace WpfApp1
 
         private void InterfaceComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            device = (from d in devices where d.Description == InterfaceComboBox.SelectedItem.ToString() select d).ToArray()[0];
+        }
 
+        private void BackgroundThread()
+        {
+            while (!backgroundThreadStop)
+            {
+                bool shouldSleep = true;
+                lock (queueLock)
+                {
+                    if (captureList.Count != 0)
+                    {
+                        shouldSleep = false;
+                    }
+                }
+                if (shouldSleep)
+                {
+                    Thread.Sleep(250);
+                }
+                else
+                {
+                    List<RawCapture> workingList;
+                    lock (queueLock)
+                    {
+                        workingList = captureList;
+                        captureList = new ();
+                    }
+
+                    foreach (var capture in workingList)
+                    {
+                        PacketRecord packetRecord = new(packetCount, capture);
+                        Dispatcher.BeginInvoke(() => { packetQueue.Enqueue(packetRecord); });
+
+                        packetCount++;
+
+                        var time = capture.Timeval.Date;
+                        var length = capture.Data.Length;
+                    }
+
+                    if (statisticsNeedsUpdate)
+                    {
+                        Dispatcher.BeginInvoke(() =>
+                        {
+                            foreach (var packet in packetQueue.Reverse())
+                            {
+                                Records.Add(packet);
+                            }
+                        });
+                        UpdateStatistics();
+                        statisticsNeedsUpdate = false;
+                    }
+                }
+            }
+        }
+
+        private void UpdateStatistics()
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                StatusText.Text = $"Received {captureStatistics.ReceivedPackets} packets, dropped {captureStatistics.DroppedPackets} packets, interface dropped {captureStatistics.InterfaceDroppedPackets} packets.";
+            });
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StopCapture();
         }
     }
 }
---
+++ WpfApp1/PacketRecord.cs
@@ -0,0 +1,36 @@
+﻿using PacketDotNet;
+using SharpPcap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class PacketRecord
+    {
+        private readonly RawCapture rawCapture;
+        public int No { get; private set; }
+        private readonly Packet packet;
+        public string SourceAddress { get; private set; }
+        public string DestinationAddress { get; private set; }
+        public string SourcePort { get; private set; }
+        public string DestinationPort { get; private set; }
+        public string Type { get; private set; }
+        public int Length { get { return rawCapture.PacketLength; } }
+        public string Timestamp { get { return rawCapture.Timeval.Date.ToString(); } }
+
+        public PacketRecord(int count, RawCapture capture)
+        {
+            rawCapture = capture;
+            No = count;
+            packet = capture.GetPacket();
+        }
+
+        private void GetDetail()
+        {
+
+        }
+    }
+}
