4cc6f5d3d7d5b78cd2fbd82e01f1357ecdff7247
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2023-03-23 18:14:55 +0800

    properly rename

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace Sniffer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
